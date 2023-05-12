91cbf8afdfd1d8dba11ebf22f9ab72a51dc0965c
shaowenchen <mail@chenshaowen.com>
2023-05-12 12:13:41 +0800

    improvement: go wasm

--- wasm-hello-world/go/main.go
+++ wasm-hello-world/go/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"syscall/js"
+)
+
 func main() {
-	println("Hello, World! by Go")
+	js.Global().Get("console").Get("log").Invoke("Hello, World by Go")
+	js.Global().Get("document").Call("getElementById", "hello").Set("innerHTML", "Hello, World! by Go")
+	js.Global().Set("myfunc", js.FuncOf(myfunc))
+	<-make(chan bool)
+}
+
+func myfunc(this js.Value, args []js.Value) interface{} {
+	println("myfunc called")
+	myfunc_arg0 := args[0].String()
+	js.Global().Set("myfunc_arg0", myfunc_arg0)
+	js.Global().Get("console").Get("log").Invoke(myfunc_arg0)
+	return nil
 }
