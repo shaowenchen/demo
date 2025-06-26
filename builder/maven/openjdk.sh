2a520029ae662d76266bc976321cecaafa83efc4
shaowenchen <mail@chenshaowen.com>
2025-06-26 15:47:18 +0800

    fix maven

--- builder/maven/openjdk.sh
+++ builder/maven/openjdk.sh
@@ -12,7 +12,7 @@ FROM shaowenchen/runtime-openjdk:$line
 RUN mkdir -p /builder && \
     mkdir -p /usr/local/maven/repository
 COPY --from=builder /temp/apache-maven-3.9.4 /usr/local/maven/apache-maven-3.9.4
-ADD https://raw.githubusercontent.com/shaowenchen/demo/main/builder/maven/settings.xml /usr/local/maven/apache-maven-3.9.4/conf/
+ADD https://raw.githubusercontent.com/shaowenchen/demo/master/builder/maven/settings.xml /usr/local/maven/apache-maven-3.9.4/conf/
 ENV MAVEN_HOME /usr/local/maven/apache-maven-3.9.4
 ENV CLASSPATH ${MAVEN_HOME}/lib:$CLASSPATH
 ENV PATH ${MAVEN_HOME}/bin:$PATH
