19da2933666e0a887a23a5765fedaa10e5ff0419
shaowenchen <mail@chenshaowen.com>
2025-06-26 13:05:43 +0800

    fix

--- runtime/ubuntu/20.04.sh
+++ runtime/ubuntu/20.04.sh
@@ -8,7 +8,7 @@ ENV TZ=Asia/Shanghai
 RUN mkdir -p /runtime && \
     apt-get -y update && \
     apt-get -y install python3-pip vim tzdata wget curl xz-utils bash gettext ca-certificates && \
-    wget https://raw.githubusercontent.com/shaowenchen/shaowenchen/main/mirror/ubuntu/20.04.focal.aliyun.sources.list -O /etc/apt/sources.list && \
+    wget https://raw.githubusercontent.com/shaowenchen/ops-hub/refs/heads/master/mirror/ubuntu/20.04.focal.aliyun.sources.list -O /etc/apt/sources.list && \
     ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
     echo "Asia/Shanghai" > /etc/timezone && \
     apt-get clean && \
