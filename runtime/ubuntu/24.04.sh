5d29f7f6b80b725087b4f26172bca91da3326bd2
shaowenchen <mail@chenshaowen.com>
2025-06-27 15:24:26 +0800

    update runtime image

---
+++ runtime/ubuntu/24.04.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t shaowenchen/runtime-ubuntu:24.04 - << EOF
+FROM ubuntu:24.04
+LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=Asia/Shanghai
+RUN mkdir -p /runtime && \
+    apt-get -y update && \
+    apt-get -y install python3-pip vim tzdata wget curl xz-utils bash gettext ca-certificates && \
+    wget https://raw.githubusercontent.com/shaowenchen/ops-hub/refs/heads/master/mirror/ubuntu/24.04.noble.aliyun.sources.list -O /etc/apt/sources.list.d/ubuntu.sources && \
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone && \
+    apt-get clean && \
+    rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
+WORKDIR /runtime
+EOF
