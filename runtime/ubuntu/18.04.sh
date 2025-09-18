a958cb44e9b139e341a5739fa8a380d8f0c0aeed
shaowenchen <mail@chenshaowen.com>
2025-09-18 17:52:15 +0800

    minor

--- runtime/ubuntu/18.04.sh
+++ runtime/ubuntu/18.04.sh
@@ -7,8 +7,8 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV TZ=Asia/Shanghai
 RUN mkdir -p /runtime && \
     apt-get -y update && \
-    apt-get -y install python3-pip vim tzdata wget curl xz-utils bash gettext ca-certificates && \
-    wget https://raw.githubusercontent.com/shaowenchen/ops-hub/refs/heads/master/mirror/ubuntu/18.04.bionic.aliyun.sources.list -O /etc/apt/sources.list && \
+    apt-get -y install python3-pip vim tzdata wget jq curl xz-utils bash gettext ca-certificates && \
+    (curl -fsSL --retry 5 --connect-timeout 10 --max-time 60 https://raw.githubusercontent.com/shaowenchen/ops-hub/refs/heads/master/mirror/ubuntu/20.04.focal.aliyun.sources.list -o /etc/apt/sources.list || wget -q --no-check-certificate https://raw.githubusercontent.com/shaowenchen/ops-hub/refs/heads/master/mirror/ubuntu/20.04.focal.aliyun.sources.list -O /etc/apt/sources.list) && \
     ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
     echo "Asia/Shanghai" > /etc/timezone && \
     apt-get clean && \
