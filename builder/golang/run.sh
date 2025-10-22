582a1ab1f3a3d172228dc608758ba108e0e487bd
shaowenchen <mail@chenshaowen.com>
2025-10-22 20:28:34 +0800

    fix golang builder

--- builder/golang/run.sh
+++ builder/golang/run.sh
@@ -8,9 +8,7 @@ LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
 RUN mkdir -p /builder && \
     go env -w GO111MODULE=on && \
     go env -w GOSUMDB=on && \
-    go env -w GOPROXY=https://mirrors.aliyun.com/goproxy,direct && \
-    wget https://github.com/swaggo/swag/releases/download/v1.16.4/swag_1.16.4_Linux_x86_64.tar.gz && \
-    tar -xvf swag_1.16.4_Linux_x86_64.tar.gz -C /bin/ && rm -rf swag*
+    go env -w GOPROXY=https://mirrors.aliyun.com/goproxy,direct
 WORKDIR /builder
 EOF
 done
